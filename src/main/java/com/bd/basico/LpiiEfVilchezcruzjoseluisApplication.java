3183839ce09067f336be2893cfd738a717b45069
Jose <Jose@gmail.com>
2024-07-04 03:15:21 -0500

    Inicio del Proyecto

---
+++ src/main/java/com/bd/basico/LpiiEfVilchezcruzjoseluisApplication.java
@@ -0,0 +1,13 @@
+package com.bd.basico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiEfVilchezcruzjoseluisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiEfVilchezcruzjoseluisApplication.class, args);
+	}
+
+}
